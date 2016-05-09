e1a7846594858a0d2941b7b004d7542aeb9af941
Marcus Nordberg <nordberg@github.com>
2016-05-09 22:09:08 +0200

    Add footer to bottom of page

--- Website.elm
+++ Website.elm
@@ -37,19 +37,30 @@ update action model =
 view : Signal.Address Action -> Model -> Html
 view address model =
   div []
-    [ div [ ] [ div [ headerContent ] [ text (model.name) ]]
-    , div [ ] [ div [ bodyContent ] [ text (toString model.age) ]]
-    , div [ ] [ div [ socialContent ] [ text ("Twitter: " ++ socialModel.twitter) ]]
+    [ div [ header ]
+      [ text (model.name) ]
+    , div [ ]
+      [ div [ bodyContent ] [ text "Welcome to my website! As you might notice, I am still settling in." ]
+      , div [ footer ] [ text "Created with Elm. I think." ]
+      ]
+  ]
+
+body : Attribute
+body =
+  style
+    [ ("display", "block")
+    , ("height", "100%")
+    , ("min-height", "100%")
     ]
 
-headerContent : Attribute
-headerContent =
+header : Attribute
+header =
   style
-    [ ("font-size", "64px")
+    [ ("font-size", "4em")
     , ("font-family", designModel.brandFont)
     , ("color", designModel.primaryColor)
     , ("display", "block")
-    , ("height", "200px")
+    , ("height", "30%")
     , ("text-align", "center")
     , ("background-color", designModel.backgroundColor)
     ]
@@ -57,17 +68,32 @@ headerContent =
 bodyContent : Attribute
 bodyContent =
   style
-    [ ("font-size", "24px")
+    [ ("font-size", "2em")
     , ("font-family", designModel.accentFont)
-    , ("display", "inline-block")
+    , ("display", "block")
     , ("text-align", "center")
     ]
 
 socialContent : Attribute
 socialContent =
   style
-    [ ("font-size", "18px")
+    [ ("font-size", "1em")
     , ("font-family", designModel.accentFont)
     , ("display", "inline-block")
     , ("text-align", "center")
+    ]
+
+footer : Attribute
+footer =
+  style
+    [ ("font-size", ".8em")
+    , ("position", "fixed")
+    , ("bottom", "0")
+    , ("right", "0")
+    , ("left", "0")
+    , ("height", "3em")
+    , ("background-color", designModel.backgroundColor)
+    , ("font-family", designModel.accentFont)
+    , ("text-align", "center")
+    , ("vertical-align", "middle")
     ]
